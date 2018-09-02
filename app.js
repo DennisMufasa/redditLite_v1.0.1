43fc65666cc37224d881445975ed7a0584cd4af3
DennisMufasa <denny.muasa@gmail.com>
2018-09-02 15:33:45 +0300

    request to add a new post on timeline

--- app.js
+++ app.js
@@ -17,6 +17,17 @@ app.set("view engine", "ejs");
 after this a static folder is created and everything inside it is accessed from root URL*/
 app.use(express.static("public"));
 
+
+const randomData =[
+    {title : "Terry Hasley", image : "/img/avril.jpg"}, //the images are linked using root URL since they are in an express static folder
+    {title : "Sarah Hackabee", image : "/img/elemnts.png"},
+    {title : "Ted Danson", image : "/img/mask.jpg"},
+    {title : "Jane Eisenhower", image : "/img/pp.jpg"},
+    {title : "Henrietta Levette", image : "/img/star.jpg"},
+    {title : "Whoompy Goldberg", image : "/img/weed.jpg"}
+]
+
+
 //route to the home page
 app.get("/", (req, res) => {
     res.render("home");
@@ -24,19 +35,28 @@ app.get("/", (req, res) => {
 
 //routing the timeline
 app.get("/timeline", (req, res) => {
-    const randomData =[
-        {title : "Terry Hasley", image : "/img/avril.jpg"}, //the images are linked using root URL since they are in an express static folder
-        {title : "Sarah Hackabee", image : "/img/elemnts.png"},
-        {title : "Ted Danson", image : "/img/mask.jpg"},
-        {title : "Jane Eisenhower", image : "/img/pp.jpg"},
-        {title : "Henrietta Levette", image : "/img/star.jpg"},
-        {title : "Whoompy Goldberg", image : "/img/weed.jpg"}
-    ]
-
+    
     //rendering the timeline.ejs and passing data to it
     res.render("timeline" , {myData : randomData});
 });
 
+//routing the new post request for uploading a new post
+app.get("/timeline/new", (req, res) => {
+    res.render("newPost");
+});
+
+//route for posting
+app.post("/timeline", (req, res) =>{
+    //capturing user input
+    let inputTitle = req.body.title;
+    let inputImage = req.body.image;
+
+    //adding the captured data to the array of posts
+    randomData.push({title : inputTitle, image : inputImage});
+    
+    //rendering the timeline with new data
+    res.redirect("/timeline");
+});
 
 //port for app to listen to
 const port = "7080";
